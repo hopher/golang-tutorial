214bda66a545e38fef9e232e7a688c88d07ced82
hopher <565983236@qq.com>
2019-04-08 17:18:17 +0800

    update files Mon Apr  8 17:18:16 CST 2019

--- code/03/03-xx.go
+++ code/03/03-xx.go
@@ -5,7 +5,22 @@ import "fmt"
 func main() {
 	
 
-	// 第一种
+	// 1. 无初始化, 指定变量类型
 	var name string
-	fmt.Printf("%s", name)
+	var age int
+	fmt.Printf("%q %d\n", name, age)
+	// 输出结果
+	// "" 0
+
+	// 2. var 类型推断
+	var name2 = "gopher2"
+	fmt.Printf("%s\n", name2)
+	// 输出结果
+	// gopher2
+
+	// 3. 短变量声明 :=
+	name3 := "gopher3"
+	fmt.Printf("%s\n", name3)
+	// 输出结果
+	// gopher3
 }
