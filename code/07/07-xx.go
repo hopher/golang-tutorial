28fefacc69f4b94e4be501c43f0f494b59a5b08d
hopher <565983236@qq.com>
2019-04-09 12:21:35 +0800

    update files Tue Apr  9 12:21:33 CST 2019

---
+++ code/07/07-xx.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a int= 20   /* 声明实际变量 */
+
+	fmt.Printf("a 变量的地址是: %#x\n", &a  )
+}
--- run.sh
+++ run.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-hugo server -D --bind=0.0.0.0 --environment dev
+#hugo server -D --bind=0.0.0.0 --environment dev
+hugo server -D --bind=0.0.0.0
